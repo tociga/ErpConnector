6af243e2418d9126404efc8edc0d27adc23df740
Björn Vignir Magnússon <bjorn@agr.is>
2017-04-28 12:26:44 +0000

    feat: add controllers

---
+++ Api/Controllers/ProductMasterController.cs
@@ -0,0 +1,22 @@
+﻿using ErpDTO.DTO;
+using System.Web.Http;
+
+namespace ErpConnector.Controllers
+{
+    [RoutePrefix("product_master")]
+    public class ProductMasterController : ApiController
+    {
+        [HttpPost]
+        [Route("")]
+        public IHttpActionResult Create([FromBody] ProductMasterWriteDTO productMaster)
+        {
+            var connector = new AxConnect.AXODataConnector();
+            var pm = connector.CreateProductMaster(productMaster);
+            if (pm != null)
+            {
+                return Ok(pm);
+            }
+            return BadRequest();
+        }
+    }
+}
