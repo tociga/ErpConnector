7f9aaa1b1ae3fed4933316bca2c3789ca5e530a6
Björn Vignir Magnússon <bjorn@agr.is>
2017-05-15 15:36:17 +0000

    feat: owin auth

---
+++ Api/Authentication/ApplicationOAuthProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ErpConnector.Authentication
+{
+    public class ApplicationOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+    }
+
+}
