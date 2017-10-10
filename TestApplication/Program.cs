f90b249c3bdc1a6f1c94bedb8842db7907934dc9
dadi <dadi@agr.is>
2017-10-10 13:25:39 +0000

    Adding test applicatin to be able to test connections to remote source

---
+++ TestApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using ErpConnector.Ax;
+using ErpConnector.Ax.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+
+            var distinctProducts = ServiceConnector.CallOdataEndpoint<DistinctProductsDTO>("DistinctProducts", "").Result.value;
+
+            Console.WriteLine("number of Distinct Products= " + distinctProducts.Count);
+        }
+    }
+}
